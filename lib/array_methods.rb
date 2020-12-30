c84c55da1a3a4d6c36cc77ed0b6f0d4d63753080
calm-collection-3916 <khalinredd@gmail.com>
2020-12-30 03:23:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,22 +5,30 @@ end
 using_include(["wow", "I", "am", "really", "learning", "arrays!"], "wow")
 
 def using_sort(array)
-  binding.pry
   array.sort 
 end
 using_sort(["wow", "I", "am", "really", "learning", "arrays!"])
+
 def using_reverse(array)
+  array.reverse 
 
 end
+using_reverse(["wow", "I", "am", "really", "learning", "arrays!"])
 
 def using_first(array)
+  array.first 
 
 end
+using_first(["wow", "I", "am", "really", "learning", "arrays!"])
 
 def using_last(array)
+  array.last 
 
 end
+using_last(["wow", "I", "am", "really", "learning", "arrays!"])
 
 def using_size(array)
+  array.size 
 
 end
+using_size(["wow", "I", "am", "really", "learning", "arrays!"])
